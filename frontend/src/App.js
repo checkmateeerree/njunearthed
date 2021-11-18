aed6a48fc9d4612c348b231ef6806e6fbb1ffe20
Alan hu <chessplayer292@gmail.com>
2021-11-17 21:12:59 -0500

    fixed frontend basic issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
-
-import './App.css';
+import Navbar from './components/BaseComponents/Navbar/Navbar';
+import Footer from './components/BaseComponents/Footer/Footer';
+import PageRouter from "./components/BaseComponents/PageRouter/PageRouter";
 
 function App() {
+  
   return (
-    <div className="App">
-      hi
-    </div>
+    <>
+      <Navbar />
+      <PageRouter />
+      <Footer />
+    </>
   );
 }
 
